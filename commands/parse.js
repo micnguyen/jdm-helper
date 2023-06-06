f7f88efbd496ca00048afe77a6d70dd0d3c130bc
Michael Nguyen <contact@micnguyen.net>
2023-06-06 22:58:15 +1000

    Update command line usage

---
+++ commands/parse.js
@@ -0,0 +1,52 @@
+import { readCsv } from "../utils/csvReader.js";
+import { renderTable } from "../utils/renderer.js";
+import { downloader } from "../utils/downloader.js";
+import appPackage from "../package.json" assert { type: "json" }; // experimental node flag to import json: https://nodejs.org/docs/latest-v16.x/api/esm.html#json-modules
+
+export const parse = async (file) => {
+  console.log(file)
+
+  console.log("Starting jdm-helper v" + appPackage.version)
+  console.log("Downloading justdeleteme.xyz sites info...")
+
+  const jdmSites = await downloader('https://raw.githubusercontent.com/jdm-contrib/jdm/master/_data/sites.json')
+  if (jdmSites) {
+    console.log("File download success")
+  }
+
+  console.log("Reading password file: " + file)
+  readCsv(file, (sitesWithAccounts) => {
+    console.log('CSV file successfully processed');
+
+     // extract a flat list of all domains 
+    const extractedJdmSites = jdmSites.map(jdmSite => {
+      return {
+        name: jdmSite.name,
+        domains: jdmSite.domains,
+        url: jdmSite.url,
+        difficulty: jdmSite.difficulty
+      }
+    })
+    const jdmDomains = extractedJdmSites.map(element => {
+      return element.domains
+    }).flat(1) // array depth level 1
+
+    // find matched domains between jdm & password
+    const matchedSites = jdmDomains.filter(jdmSite => sitesWithAccounts.includes(jdmSite))
+
+    // get all jdm info for sites that users have accounts to
+    const result = extractedJdmSites.filter((parsedEntry) => {
+      const domains = parsedEntry.domains
+      const userHasAccount = domains.filter(jdm => matchedSites.includes(jdm)).length > 0
+      return userHasAccount
+    })
+
+    console.log("Rendering output...")
+    renderTable({
+      appVersion: appPackage.version,
+      passwordManager: "1Password", // TODO: Detect password manager off filename
+      filename: file,
+      inputData: result
+    })
+  })
+}
--- index.js
+++ index.js
@@ -1,70 +1,17 @@
 #! /usr/bin/env node
-import { list } from "./commands/list.js";
-import { add } from "./commands/add.js";
-import { markDone } from "./commands/markDone.js";
-import { downloader } from "./utils/downloader.js";
+import { parse } from "./commands/parse.js"
 import { Command } from 'commander';
-import { parse } from 'csv-parse';
-import { readCsv } from "./utils/csvReader.js";
-import { stripDomain } from "./utils/regexp.js";
-import { renderTable } from "./utils/renderer.js";
 import appPackage from "./package.json" assert { type: "json" };
 
 const program = new Command();
 
-console.log("Starting jdm-helper v" + appPackage.version)
-console.log("Downloading justdeleteme.xyz sites info...")
-const jdmSites = await downloader('https://raw.githubusercontent.com/jdm-contrib/jdm/master/_data/sites.json')
-const parsedJdmSites = jdmSites.map(jdmSite => {
-  return {
-    name: jdmSite.name,
-    domains: jdmSite.domains,
-    url: jdmSite.url,
-    difficulty: jdmSite.difficulty
-  }
-})
-
-readCsv('./test_data/1PasswordExport-3CJPZFQNXBDCXEZCUYJQ2J2KOM-20230605-101912.csv', (sitesWithAccounts) => {
-  console.log('CSV file successfully processed');
-
-  // extract a flat list of all domains 
-  const jdmDomains = parsedJdmSites.map(element => {
-    return element.domains
-  }).flat(1)
-
-  // find matched domains between jdm & password
-  const matchedSites = jdmDomains.filter(jdmSite => sitesWithAccounts.includes(jdmSite))
-
-  // get all jdm info for sites that users have accounts to
-  const result = parsedJdmSites.filter((parsedEntry) => {
-    const domains = parsedEntry.domains
-    const userHasAccount = domains.filter(jdm => matchedSites.includes(jdm)).length > 0
-    return userHasAccount
-  })
-
-  console.log("Rendering output...")
-  renderTable({
-    appVersion: appPackage.version,
-    passwordManager: "1Password",
-    filename: "filename.csv",
-    inputData: result
-  })
-})
+program.name('jdm-helper')
+  .description("CLI tool to help identify what online accounts you have that can be deleted")
+  .version(appPackage.version)
 
 program
-  .command('list')
-  .description('Sync')
-  .action(list)
-
-program
-  .command('add <task>')
-  .description('Add a new TODO task')
-  .action(add)
-
-program
-  .command('mark-done')
-  .description('Mark commands done')
-  .option('-t, --tasks <tasks...>', 'The tasks to mark done. If not specified, all tasks will be marked done.')
-  .action(markDone)
+  .argument('<file>')
+  .description('Reads an exported CSV file from a password manager')
+  .action(parse)
 
 program.parse()
